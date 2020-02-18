b6dda4c64bd5fca835c9373c61fd85e51f677bf8
Peter Ihme <peter.ihme@camunda.com>
2020-02-18 13:00:46 +0100

    trial and error: figure out how to feed neutral config file into docker container

--- docker/utils/startup.sh
+++ docker/utils/startup.sh
@@ -1,6 +1,6 @@
 #!/bin/bash -xeu
 
-configFile=/usr/local/zeebe/conf/zeebe.cfg.toml
+configFile=/usr/local/zeebe/conf/zeebe.cfg.yaml
 
 export ZEEBE_HOST=${ZEEBE_HOST:-$(hostname -i)}
 export ZEEBE_GATEWAY_CLUSTER_HOST=${ZEEBE_GATEWAY_CLUSTER_HOST:-${ZEEBE_HOST}}
